939f006d893ea83a76597165e8bc6225d49c11e0
Joziélio Santiago <jozieliosantiago@gmail.com>
2019-10-11 13:52:43 +0000

    Alterações no layout

--- src/main/mobileapp/www/js/dataSourceMap.js
+++ src/main/mobileapp/www/js/dataSourceMap.js
@@ -1,12 +1,1 @@
-window.dataSourceMap = window.dataSourceMap || [];
-window.dataSourceMap["app.device"] = { customId: "device", serviceUrl: "api/cronapi/query/1jy88nt41afza/", serviceUrlODATA: "/api/cronapi/odata/v2/app/device" };
-window.dataSourceMap["app.User"] = { customId: "app.entity.User", serviceUrl: "api/cronapi/crud/app.entity.User", serviceUrlODATA: "/api/cronapi/odata/v2/app/User" };
-window.dataSourceMap["app.auth"] = { customId: "auth", serviceUrl: "api/cronapi/query/auth/", serviceUrlODATA: "/api/cronapi/odata/v2/app/auth" };
-window.dataSourceMap["app.roles"] = { customId: "roles", serviceUrl: "api/cronapi/query/roles/", serviceUrlODATA: "/api/cronapi/odata/v2/app/roles" };
-window.dataSourceMap["app.Device"] = { customId: "app.entity.Device", serviceUrl: "api/cronapi/crud/app.entity.Device", serviceUrlODATA: "/api/cronapi/odata/v2/app/Device" };
-window.dataSourceMap["app.AlunoAvaliacaoNota"] = { customId: "app.entity.AlunoAvaliacaoNota", serviceUrl: "api/cronapi/crud/app.entity.AlunoAvaliacaoNota", serviceUrlODATA: "/api/cronapi/odata/v2/app/AlunoAvaliacaoNota" };
-window.dataSourceMap["app.Turma"] = { customId: "app.entity.Turma", serviceUrl: "api/cronapi/crud/app.entity.Turma", serviceUrlODATA: "/api/cronapi/odata/v2/app/Turma" };
-window.dataSourceMap["app.Avaliacao"] = { customId: "app.entity.Avaliacao", serviceUrl: "api/cronapi/crud/app.entity.Avaliacao", serviceUrlODATA: "/api/cronapi/odata/v2/app/Avaliacao" };
-window.dataSourceMap["app.Role"] = { customId: "app.entity.Role", serviceUrl: "api/cronapi/crud/app.entity.Role", serviceUrlODATA: "/api/cronapi/odata/v2/app/Role" };
-window.dataSourceMap["app.query960928"] = { customId: "query960928", serviceUrl: "api/cronapi/query/query960928/", serviceUrlODATA: "/api/cronapi/odata/v2/app/query960928" };
-window.dataSourceMap["app.Aluno"] = { customId: "app.entity.Aluno", serviceUrl: "api/cronapi/crud/app.entity.Aluno", serviceUrlODATA: "/api/cronapi/odata/v2/app/Aluno" };
+
